86d2070e6876dbe5b2619b2c74aeaa4934c9b888
ism <vladimir.ismagilov@ligastavok.ru>
2017-10-29 18:23:02 +0300

    Урок 2, задание 3. Тест авторизации

---
+++ Lesson2.3/src/main/java/LoginTest.java
@@ -0,0 +1,28 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+    public WebDriver driver;
+
+    @Before
+    public void beforeTest(){
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void loginTest(){
+        driver.navigate().to("http://j763976.myjino.ru/admin");
+        driver.findElement(By.name("username")).sendKeys("admin"); // вводим логин
+        driver.findElement(By.name("password")).sendKeys("ololo_admin"); // вводим пароль
+        driver.findElement(By.name("login")).click(); // жмем кнопку входа
+    }
+
+    @After
+    public void afterTest(){
+        driver.quit();
+    }
+}
